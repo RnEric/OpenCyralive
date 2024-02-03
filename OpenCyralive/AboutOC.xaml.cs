02303a0f19d44b3c3f315e1412d8ca1e7cdfb36c
Eric Ren <ericrren@outlook.com>
2024-02-03 11:54:22 +1100

    Update to 2.5

--- OpenCyralive/AboutOC.xaml.cs
+++ OpenCyralive/AboutOC.xaml.cs
@@ -26,6 +26,11 @@ namespace OpenCyralive
             InitializeComponent();
             object[] all_author = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCompanyAttribute), false);
             oc_info.Text = "\n\n作者: " + ((AssemblyCompanyAttribute)all_author[0]).Company + "\n版本: " + Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            string[] strings = { "pack://application:,,,/res/Bluelines.jpg", "pack://application:,,,/res/Starsky.jpg" };
+            ImageBrush imageBrush = new ImageBrush();
+            imageBrush.Stretch = Stretch.UniformToFill;
+            imageBrush.ImageSource = new BitmapImage(new Uri(strings[new Random().Next(0, strings.Length)]));
+            Background = imageBrush;
         }
 
         private void aboutOC_close_Click(object sender, RoutedEventArgs e)
