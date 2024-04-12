4ed95ba9eb60e75a8f6c2f24bc918c46bd4f4905
Eric Ren <ericrren@outlook.com>
2024-04-12 17:18:26 +0800

    Cleanup code.

--- OpenCyralive/ocSettings.xaml.cs
+++ OpenCyralive/ocSettings.xaml.cs
@@ -1,21 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Text.Json.Nodes;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using static OpenCyralive.GlobalFunction;
 using Application = System.Windows.Application;
 using ColorConverter = System.Drawing.ColorConverter;
@@ -24,9 +16,7 @@ using ComboBox = System.Windows.Controls.ComboBox;
 using System.Reflection;
 using IWshRuntimeLibrary;
 using File = System.IO.File;
-using Microsoft.VisualBasic;
 using MessageBox = System.Windows.Forms.MessageBox;
-using Wpf.Ui.Controls;
 
 namespace OpenCyralive
 {
